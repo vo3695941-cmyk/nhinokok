63ee9b3c43de30b2758b22d13bd6510dae857585
vo3695941-cmyk <vo3695941@gmail.com>
2026-08-04 20:58:48 +0700

    Increase max levels from 3 to 5 and update maps

--- map.js
+++ map.js
@@ -1,18 +1,56 @@
-// Khai báo số màn chơi tối đa
-const maxLevels = 3;
+// Khai báo số màn chơi tối đa đã nâng cấp lên 5 màn
+const maxLevels = 5;
 
-// Định nghĩa ma trận sơ đồ tường đỏ của từng màn chơi đơn lẻ
+// Định nghĩa ma trận sơ đồ tường đỏ của 5 màn chơi riêng biệt
 const gameLevelsData = {
     1: {
-        mapStr: "11111111" + "10000001" + "10000001" + "10000001" + "10000001" + "11111111",
+        // Màn 1: Phòng trống vuông vắn cơ bản
+        mapStr: "11111111" + 
+                "10000001" + 
+                "10000001" + 
+                "10000001" + 
+                "10000001" + 
+                "11111111",
         playerStart: { x: 60, y: 60, angle: 0 }
     },
     2: {
-        mapStr: "11111111" + "10000001" + "10111101" + "10000101" + "10000001" + "11111111",
+        // Màn 2: Bức tường dài chia đôi phòng ở giữa
+        mapStr: "11111111" + 
+                "10000001" + 
+                "10111101" + 
+                "10000101" + 
+                "10000001" + 
+                "11111111",
         playerStart: { x: 60, y: 60, angle: 0 }
     },
     3: {
-        mapStr: "11111111" + "10001001" + "10101011" + "10100001" + "10111101" + "11111111",
+        // Màn 3: Mê cung nhỏ lắt léo
+        mapStr: "11111111" + 
+                "10001001" + 
+                "10101011" + 
+                "10100001" + 
+                "10111101" + 
+                "11111111",
+        playerStart: { x: 60, y: 60, angle: 0 }
+    },
+    4: {
+        // Màn 4: Thiết kế phòng có 4 cột trụ lớn chắn ở các góc
+        mapStr: "11111111" + 
+                "10100101" + 
+                "10000001" + 
+                "10000001" + 
+                "10100101" + 
+                "11111111",
+        playerStart: { x: 60, y: 120, angle: 0 }
+    },
+    5: {
+        // Màn 5: Mê cung hầm hẹp hình chữ Z (Màn khó nhất)
+        mapStr: "11111111" + 
+                "10000011" + 
+                "11110001" + 
+                "10000111" + 
+                "11000001" + 
+                "11111111",
         playerStart: { x: 60, y: 60, angle: 0 }
     }
 };
